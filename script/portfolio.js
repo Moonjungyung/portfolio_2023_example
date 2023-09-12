5033ffd004216ed1b2559daca6a9a430c348adb1
jungyung <moonjune58601@gmail.com>
2023-09-12 17:13:49 +0900

    edit

---
+++ script/portfolio.js
@@ -0,0 +1,18 @@
+const cardBtn = document.querySelectorAll('.title .btn button')
+const cardContent = document.querySelectorAll('#card ul')
+const card2 = document.querySelector('#card .arbor')
+const card3 = document.querySelector('#card .vegetarian')
+console.log(cardContent, cardBtn)
+
+card2.style.display = 'none'
+card3.style.display = 'none'
+
+cardBtn.forEach(function(target, index, array){
+    target.addEventListener('click',function(){
+        for(let i of cardBtn){i.classList.remove('active')}
+        target.classList.add('active')
+        console.log(index)
+        for(let i of cardContent){i.style.display = 'none'}
+        cardContent[index].style.display = 'flex'
+    })
+})
